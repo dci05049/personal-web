f7a52a9ee453d7ff5be7c70824007c6bf8f11c99
Jason Yang <jy_jason98@hotmail.com>
2018-05-23 17:28:02 -0400

    remove turbolink

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require prism
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require react
 //= require react_ujs
 //= require components
+//= require prism
 //= require_tree .
